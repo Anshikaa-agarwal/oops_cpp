9a88fb3ebba0131d6fedb57867b1b4d85ad5dfc8
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-07-28 10:42:02 +0530

    friend function and clasS

---
+++ frndFunc.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class first {
+    int age;
+    int roll;
+    friend void accessFirst(first);
+    friend class Second;
+
+public:
+    first() {
+        age = 20;
+        roll = 3;
+    }
+};
+
+class Second {
+public:
+    void printDetails(first f) {
+        cout << "age: " << f.age << endl;
+        cout << "roll: " << f.roll << endl;
+    }
+};
+
+void accessFirst(first f) {
+    cout << "age: " << f.age << endl;
+    cout << "roll: " << f.roll << endl;
+}
+
+int main() {
+    cout << "Using friend function:" << endl;
+    first f1;
+    accessFirst(f1);
+
+    cout << "Using friend class:" << endl;
+    Second s1;
+    s1.printDetails(f1);
+    return 0;
+}
